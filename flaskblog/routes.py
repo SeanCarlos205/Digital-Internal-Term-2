387121507a24879b37f5f0f47f9ba6e59ce02ed6
SeanCarlos205 <16045@my.sanctamaria.school.nz>
2022-06-13 12:31:36 +1200

    Fixed routes page
    The home route wasn't working properly so i added some code to fix it and delete the site.db. I'll need to do that page agian

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -13,6 +13,8 @@ from flask_login import login_user, current_user, current_user, logout_user, log
 @app.route("/")
 @app.route("/home")
 def home():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 #About page
